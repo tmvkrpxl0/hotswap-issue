d2260905406f5cb1834c5a0958322eef8d40bf2e
tmvkrpxl0 <ironman466@naver.com>
2022-10-04 03:04:59 +0900

    Initial commit

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,6 +1,12 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,5 +23,9 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		Item.Settings settings = new FabricItemSettings();
+		settings.group(ItemGroup.BUILDING_BLOCKS);
+		Item item = new I(settings);
+		Registry.register(Registry.ITEM, new Identifier("modid", "i"), item);
 	}
 }
---
+++ src/main/java/net/fabricmc/example/I.java
@@ -0,0 +1,15 @@
+package net.fabricmc.example;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.StackReference;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.ClickType;
+
+public class I extends Item {
+    public I(Settings settings) {
+        super(settings);
+    }
+
+}
